5e61ecf833a71597623ae977d896b414fa72bf3d
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-01-16 11:37:08 +0100

    Add files via upload

---
+++ Day11.py
@@ -0,0 +1,5 @@
+print("Hello. So basically I'm going to be converting years to seconds")
+years= int(input("Value: "))
+years_to_sec= years*365*24*60
+leap_year= years*366*24*60
+print(str(years_to_sec)+" seconds")
