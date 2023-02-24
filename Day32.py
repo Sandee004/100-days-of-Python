47368a206a40630318da88ca0c66bdef3a020ef1
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-02-24 15:16:37 +0100

    Add files via upload

---
+++ Day32.py
@@ -0,0 +1,5 @@
+import random
+languages= ["e kaaro", "ụtụtụ ọma", "Bonjour", "Hola", "Zdravstvuyte", "Nǐn hǎo", "Ciao", "Konnichiwa", "Guten Tag", "Oi", "Anyoung", "Asalaam alaikum"
+"Habari", "Goddag", "Yassas", "Selamat siang", "Shalom"]
+greet = random.randint(0, 16)
+print("Hello - ", languages[greet])
