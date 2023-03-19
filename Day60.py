d43ea29d4593c026d675606fcda6cb7f49df3ea5
Sandee <102047643+Sandee004@users.noreply.github.com>
2023-03-19 03:42:51 +0100

    Add files via upload

---
+++ Day60.py
@@ -0,0 +1,19 @@
+import datetime
+
+today = datetime.date.today()
+
+print("EVENT COUNTDOWN")
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
+event = datetime.date(year, month, day)
+
+difference = event - today
+difference = difference.days
+
+if difference>0:
+  print(f"{difference} days to go")
+elif difference<0:
+  print(f"😭😭😭😭😭😭😭 You missed it by {difference} days!")
+else:
+  print("🥳🥳🥳🥳🥳🥳🥳 Today!")
